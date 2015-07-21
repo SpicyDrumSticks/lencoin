c809a4057a01e020df1aeda5099df3c8e683d9c1
lencoin <lencoin@users.noreply.github.com>
2015-07-22 01:34:04 +0300

    Lencoin: LEN and Cents only
    Todo: Remove case uBTC from everywhere and rename case mBTC to cBTC.

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -13,7 +13,6 @@ QList<BitcoinUnits::Unit> BitcoinUnits::availableUnits()
     QList<BitcoinUnits::Unit> unitlist;
     unitlist.append(BTC);
     unitlist.append(mBTC);
-    unitlist.append(uBTC);
     return unitlist;
 }
 
@@ -23,7 +22,6 @@ bool BitcoinUnits::valid(int unit)
     {
     case BTC:
     case mBTC:
-    case uBTC:
         return true;
     default:
         return false;
@@ -34,10 +32,10 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("BLK");
-    case mBTC: return QString("mBLK");
-    case uBTC: return QString::fromUtf8("μBLK");
+    case BTC: return QString("LEN");
+    case mBTC: return QString("Cents");
     default: return QString("???");
+    
     }
 }
 
@@ -45,9 +43,8 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("BlackCoins");
-    case mBTC: return QString("Milli-BlackCoins (1 / 1,000)");
-    case uBTC: return QString("Micro-BlackCoins (1 / 1,000,000)");
+    case BTC: return QString("LenCoins");
+    case mBTC: return QString("LenCoin-Cents (1 / 100)");
     default: return QString("???");
     }
 }
@@ -56,10 +53,9 @@ qint64 BitcoinUnits::factor(int unit)
 {
     switch(unit)
     {
-    case BTC:  return 100000000;
-    case mBTC: return 100000;
-    case uBTC: return 100;
-    default:   return 100000000;
+    case BTC:  return 10000;
+    case mBTC: return 100;
+    default:   return 10000;
     }
 }
 
@@ -67,9 +63,8 @@ int BitcoinUnits::amountDigits(int unit)
 {
     switch(unit)
     {
-    case BTC: return 8; // 21,000,000 (# digits, without commas)
-    case mBTC: return 11; // 21,000,000,000
-    case uBTC: return 14; // 21,000,000,000,000
+    case BTC: return 4; // 10,000 (# zeros, without commas)
+    case mBTC: return 6; // 1,000,000
     default: return 0;
     }
 }
@@ -78,9 +73,8 @@ int BitcoinUnits::decimals(int unit)
 {
     switch(unit)
     {
-    case BTC: return 8;
-    case mBTC: return 5;
-    case uBTC: return 2;
+    case BTC: return 4;
+    case mBTC: return 2;
     default: return 0;
     }
 }

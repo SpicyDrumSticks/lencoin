8d1f504452ad9030205a0a4f26d3e605159b94f1
lencoin <lencoin@users.noreply.github.com>
2015-07-22 12:52:33 +0300

    Lencoin: Proof of stake reward 1 coin now.

--- src/main.cpp
+++ src/main.cpp
@@ -970,7 +970,7 @@ int64_t GetProofOfWorkReward(int64_t nFees)
 // miner's coin stake reward
 int64_t GetProofOfStakeReward(const CBlockIndex* pindexPrev, int64_t nCoinAge, int64_t nFees)
 {
-    int64_t nSubsidy = COIN * 5;
+    int64_t nSubsidy = COIN * 1;
     
     return nSubsidy + nFees;
 }

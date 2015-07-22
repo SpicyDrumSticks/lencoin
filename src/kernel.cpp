d5c2f3e9a8032df04a6d93b10abdd1b46c0866f2
lencoin <lencoin@users.noreply.github.com>
2015-07-22 07:47:48 +0300

    synced?

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -2,7 +2,7 @@
 // Copyright (c) 2014 The BlackCoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
+//
 #include <boost/assign/list_of.hpp>
 
 #include "kernel.h"
